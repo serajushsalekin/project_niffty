216363d64f5743b1df033cbcca83470e9b0809e6
salekin <serajushsalekin@gmail.com>
2020-11-05 12:50:42 +0600

    [ADD] Fetch data from history endpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,44 @@ import React, {Fragment} from 'react'
 import axios from "axios";
 
 class App extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            output: null,
+            history: null
+        }
+    }
+    retriveHistory = e => {
+        let value = ''
+        try {
+            value = e.target.value
+        }
+        catch (e) {
+
+        }
+        if (value) {
+            axios.get(`http://127.0.0.1:8000/actions/?n=${value}`).then(res => {
+                this.setState({
+                    ...this.state,
+                    history: res.data
+                })
+            }).catch(err=> err)
+        }
+        else {
+            axios.get('http://127.0.0.1:8000/actions/')
+                .then(res => {
+                    this.setState({
+                        ...this.state,
+                        history: res.data
+                    })
+                }).catch(err => err)
+        }
+    }
 
 
     render() {
     return(
-        <Fragment>
+<Fragment>
       <div className="container m-5">
           <div className="row border border-dark p-3">
               <div className="align-items-center justify-content-between col-sm-5 col-md-6 d-flex flex-column">
@@ -31,19 +64,30 @@ class App extends React.Component{
      <div className={'row p-3'}>
           <div className={'col-sm-5 col-md-6 d-flex justify-content-center'}>
                   <div className={'d-flex'}>
-                    <button className={'btn btn-secondary btn-lg btn-light border-dark m-1'}>Show History</button>
+                    <button
+                        onClick={()=> this.retriveHistory()}
+                        className={'btn btn-secondary btn-lg btn-light border-dark m-1'}
+                    >
+                        Show History
+                    </button>
                   </div>
           </div>
          <div className={'col-sm-5 col-md-6 d-flex justify-content-center'}>
              <div className={'d-flex'}>
-                 <input type='text' className='form-control' />
+                 <input type='text' className='form-control' onChange={e=> this.retriveHistory(e)} />
              </div>
          </div>
      </div>
     </div>
-    <div className="container m-5 d-flex justify-content-center table-secondary">I'm a flexbox container!</div>
-    <div className="container m-5 d-flex justify-content-center table-secondary">I'm a flexbox container!</div>
-    <div className="container m-5 d-flex justify-content-center table-secondary">I'm a flexbox container!</div>
+    {this.state.history ? this.state.history.map(h =>
+        (<div
+            className="container m-5 d-flex justify-content-center table-secondary"
+        >
+            Operation Name: {h.operation_name} | Time: {new Date(h.time).toString()}
+        </div>)
+        )
+
+        : ''}
 
 </Fragment>
     )
